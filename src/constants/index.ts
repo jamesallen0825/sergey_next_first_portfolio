c955fd2496a5009427783a4d9af91a201803fecf
jamesallen <sfaster825@gmail.com>
2025-02-28 16:04:58 +0800

    repair date

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -145,7 +145,7 @@ export const EXPERIENCES = [
     company_name: "Starbucks",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "March 2020 - Sep 2022",
     points: [
       "Developing and maintaining web applications using React.js and other related technologies.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
